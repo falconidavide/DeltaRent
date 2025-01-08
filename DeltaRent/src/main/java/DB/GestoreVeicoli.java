b99cc3cdddf6907bc21a6b3d383a4e296fac8451
Davide Falconi <d.falconi2@studenti.unibg.it>
2025-01-08 17:01:19 +0100

    Finita logica caricamento veicoli

--- DeltaRent/src/main/java/DB/GestoreVeicoli.java
+++ DeltaRent/src/main/java/DB/GestoreVeicoli.java
@@ -9,36 +9,64 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import Veicolo.Automobile;
+import Veicolo.Furgone;
 import Veicolo.Veicolo;
 
 public class GestoreVeicoli {
-	
+
 	Connection conn = DatabaseConnection.getConnection();
-	List<Veicolo> veicoli = new ArrayList<>();
-	
+	public List<Veicolo> automobili = new ArrayList<>();
+	public List<Veicolo> furgoni = new ArrayList<>();
+
 	public List<Veicolo> getListaAutomobili() {
 		try {
-			String query = "SELECT targa, marca, modello, disponibile FROM veicoli";
+			String query = "SELECT targa, marca, modello, disponibile, prezzoOrario FROM veicoli WHERE isFurgone=0";
 			try (Statement stmt = conn.createStatement(); ResultSet rs = stmt.executeQuery(query)) {
 				while (rs.next()) {
 					String targa = rs.getString("targa");
 					String marca = rs.getString("marca");
 					String modello = rs.getString("modello");
-					Boolean disponibile = rs.getBoolean("disponibile");
-					Veicolo veicolo = new Veicolo(targa, marca, modello, disponibile);
-					veicoli.add(veicolo);
+					boolean disponibile = rs.getBoolean("disponibile");
+					int prezzoOrario = rs.getInt("prezzoOrario");
+					Automobile auto = new Automobile(targa, marca, modello, disponibile, prezzoOrario);
+					automobili.add(auto);
 				}
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-        // Stampa la lista dei veicoli per verificare
-        for (Veicolo veicolo : veicoli) {
-            System.out.println("Targa: " + veicolo.getTarga() + 
-                               ", Marca: " + veicolo.getMarca() + 
-                               ", Modello: " + veicolo.getModello() + 
-                               ", Disponibile: " + veicolo.getDisponibile());
-        }
-		return null;
+		// Stampa la lista dei veicoli per verificare
+		for (Veicolo veicolo : automobili) {
+			System.out.println("Targa: " + veicolo.getTarga() + ", Marca: " + veicolo.getMarca() + ", Modello: "
+					+ veicolo.getModello() + ", Disponibile: " + veicolo.getDisponibile());
+		}
+		return automobili;
+	}
+	
+	
+	public List<Veicolo> getListaFurgoni() {
+		try {
+			String query = "SELECT targa, marca, modello, disponibile, prezzoGiornaliero FROM veicoli WHERE isFurgone=1";
+			try (Statement stmt = conn.createStatement(); ResultSet rs = stmt.executeQuery(query)) {
+				while (rs.next()) {
+					String targa = rs.getString("targa");
+					String marca = rs.getString("marca");
+					String modello = rs.getString("modello");
+					boolean disponibile = rs.getBoolean("disponibile");
+					int prezzoGiornaliero = rs.getInt("prezzoGiornaliero");
+					Furgone furgone = new Furgone(targa, marca, modello, disponibile, prezzoGiornaliero);
+					furgoni.add(furgone);
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		// Stampa la lista dei veicoli per verificare
+		for (Veicolo veicolo : furgoni) {
+			System.out.println("Targa: " + veicolo.getTarga() + ", Marca: " + veicolo.getMarca() + ", Modello: "
+					+ veicolo.getModello() + ", Disponibile: " + veicolo.getDisponibile());
+		}
+		return furgoni;
 	}
 }
--- DeltaRent/src/main/java/GUI/Main.java
+++ DeltaRent/src/main/java/GUI/Main.java
@@ -1,7 +1,14 @@
 package GUI;
 
+import DB.GestoreVeicoli;
+
 public class Main {
     public static void main(String[] args) {
         new HomePage().setVisible(true);
+        GestoreVeicoli g = new GestoreVeicoli();
+        System.out.println("-----------AUTOMOBILI----------");
+        g.getListaAutomobili();
+        System.out.println("------------FURGONI------------");
+        g.getListaFurgoni();
     }
 }
