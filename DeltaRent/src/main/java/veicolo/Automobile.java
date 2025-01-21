fb336d636caa4dce0fcb015b89a0bee8fccc31f0
Abirolini11 <a.birolini6@studenti.unibg.it>
2025-01-21 13:51:36 +0100

    aggiunta classe Automobile generata da papyrus

--- DeltaRent/src/main/java/veicolo/Automobile.java
+++ DeltaRent/src/main/java/veicolo/Automobile.java
@@ -16,10 +16,18 @@ public class Automobile extends Veicolo
 				 * 
 				 */
 				private int prezzoOrario;
-	/**
+				public Automobile(String targa, String marca, String modello, int prezzoO, String pathImg, String alimentazione, String[] pathImgs, String descrizione) {
+					super(targa, marca, modello, pathImg, alimentazione, pathImgs, descrizione);
+					this.prezzoOrario = prezzoO;
+				}
+	/**			
 	 * 
 	 * @return 
 	 */
 	public int getPrezzoOrario() {
+		return prezzoOrario;
+	}
+	public void setPrezzoOrario(int prezzoOrario) {
+		this.prezzoOrario = prezzoOrario;
 	}
 }
