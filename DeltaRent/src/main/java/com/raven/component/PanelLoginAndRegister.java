f39d8ebb6c12cb7244f9ebdb2d5d941dc0338c07
Davide Falconi <d.falconi2@studenti.unibg.it>
2025-01-28 20:36:50 +0100

    rimossi import inutilizzati

--- DeltaRent/src/main/java/com/raven/component/PanelLoginAndRegister.java
+++ DeltaRent/src/main/java/com/raven/component/PanelLoginAndRegister.java
@@ -8,12 +8,10 @@ import my.components.MyPasswordField;
 import my.components.MyTextField;
 
 import java.awt.Color;
-import java.awt.Cursor;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
