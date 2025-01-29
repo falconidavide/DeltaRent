415bc7bf67470a4c1cb5a316ae0bd7b0c9e8389c
Davide Falconi <d.falconi2@studenti.unibg.it>
2025-01-29 13:26:24 +0100

    reso caricamento schermata home asincrono all'avvio dell'applicazione

--- DeltaRent/src/main/java/gui/Main.java
+++ DeltaRent/src/main/java/gui/Main.java
@@ -2,10 +2,15 @@ package gui;
 
 public class Main {
 	public static void main(String[] args) {
+		//HomePage homePage;
 		final PaginaCaricamento paginaCaricamento = PaginaCaricamento.getInstance();
 		paginaCaricamento.setVisible(true);
-		final HomePage homePage = new HomePage();
-		homePage.setVisible(true);
-		paginaCaricamento.dispose();
+		new Thread(new Runnable() {
+			public void run() {
+				HomePage homePage = new HomePage();
+				homePage.setVisible(true);
+				paginaCaricamento.dispose();
+			}
+		}).start();
 	}
 }
