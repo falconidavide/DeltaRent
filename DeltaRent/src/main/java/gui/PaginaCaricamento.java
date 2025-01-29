365dadffd0971d365c65514648f7ad19b617c297
Davide Falconi <d.falconi2@studenti.unibg.it>
2025-01-29 20:41:20 +0100

    rimosso import non utilizzato

--- DeltaRent/src/main/java/gui/PaginaCaricamento.java
+++ DeltaRent/src/main/java/gui/PaginaCaricamento.java
@@ -5,7 +5,6 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.Image;
 import java.awt.Insets;
 import java.util.Timer;
 import java.util.TimerTask;
